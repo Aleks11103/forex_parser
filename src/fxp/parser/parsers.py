569975096af116a1beb50fa1c3410dac2a749454
Aleksei Smantser <lesha_smancer@mail.ru>
2020-12-27 14:05:20 +0300

    parser for preview

--- src/fxp/parser/parsers.py
+++ src/fxp/parser/parsers.py
@@ -5,7 +5,7 @@ import requests
 from bs4 import BeautifulSoup as BS
 from abc import ABC, abstractmethod
 
-from fxp.parser import DEFAULT_USER_AGENT, HOST, PREVIEW_URL
+from fxp.parser import DEFAULT_USER_AGENT, HOST, PREVIEW_URL, BASE_DIR
 
 class BaseParser(ABC):
     def __init__(self, user_agent:str = None):
@@ -14,12 +14,10 @@ class BaseParser(ABC):
     def _get_page(self, url):
         response = requests.get(
             url, 
-            headers={
-                "User-Agent": self._user_agent
-            }
+            headers={"User-Agent": self._user_agent}
         )
         if response.status_code == 200:
-            return BS(response.text, features="lxml")
+            return BS(response.text, features="html.parser")
         raise ValueError("Response not 200")
 
     @abstractmethod
@@ -47,24 +45,34 @@ class Preview(BaseParser):
     def get_links(self):
         try:
             html = self._get_page(PREVIEW_URL.format(HOST, self.__num_page))
-        except ValueError:
+        except ValueError as error:
+            print(error)
             self.__links = []
         else:
             box = html.find("div", attrs={"class": "largeTitle"})
             if box is not None:
-                articles = box.find_all("article", attrs={"class": "articleItem"})
+                articles = box.find_all(
+                    "article", attrs={"class": "js-article-item articleItem"}
+                )
                 for article in articles:
                     link = article.find("a", attrs={"class": "title"})
-                    self.__links.append(link.get("href"))
+                    self.__links.append(HOST + link.get("href"))
             else:
                 self.__links = []
 
     def save_to_file(self, name):
-        pass
+        path = os.path.join(BASE_DIR, name + '.bin')
+        pickle.dump(self.__links, open(path, 'wb'))
 
     def save_to_json(self, name):
-        pass
+        path = os.path.join(BASE_DIR, name + '.json')
+        json.dump(self.__links, open(path, 'w'))
 
 
 if __name__ == "__main__":
-    parser = Preview
+    parser = Preview(page=2)
+    parser.get_links()
+    for i in parser._Preview__links:
+        print(i)
+    parser.save_to_json('links_2')
+    parser.save_to_file('links_2')
