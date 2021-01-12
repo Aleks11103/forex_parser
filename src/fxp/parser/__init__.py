0e1f25a8cc3b7ac388e52d8dee79dbf68c131f2c
Aliaksei <lesha_smancer@mail.ru>
2021-01-12 21:09:15 +0300

    add metaclass

--- src/fxp/parser/__init__.py
+++ src/fxp/parser/__init__.py
@@ -1,5 +1,7 @@
 import os
 
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
 DEFAULT_USER_AGENT = (
     "Mozilla/5.0 (Windows NT 10.0; Win64; "
     "x64) AppleWebKit/537.36 (KHTML, like Gecko)"
